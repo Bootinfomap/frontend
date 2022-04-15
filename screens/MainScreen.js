41d1676516f313ad720e65462eb555824b594db4
riroan <riroan@naver.com>
2022-04-15 21:10:42 +0900

    메뉴 수정

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import MapScreen from './MapScreen';
-import RegisterScreen from './RegisterScreen';
 import FeedScreen from './FeedScreen';
 import UserScreen from './UserScreen';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -16,25 +15,16 @@ export default function MainScreen() {
         options={{
           tabBarLabel: '지도',
           tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
-          tabBarColor: 'black',
+          tabBarColor: 'blue',
         }}
         component={MapScreen}
-      />
-      <Tab.Screen
-        name="Register"
-        options={{
-          tabBarLabel: '등록',
-          tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
-          tabBarColor: 'black',
-        }}
-        component={RegisterScreen}
       />
       <Tab.Screen
         name="Feed"
         options={{
           tabBarLabel: '피드',
           tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
-          tabBarColor: 'black',
+          tabBarColor: 'blue',
         }}
         component={FeedScreen}
       />
@@ -43,7 +33,7 @@ export default function MainScreen() {
         options={{
           tabBarLabel: '사용자',
           tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
-          tabBarColor: 'black',
+          tabBarColor: 'blue',
         }}
         component={UserScreen}
       />
