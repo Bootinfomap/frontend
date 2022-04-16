ce419412b26568160467fb46927a198d1d86fbd2
riroan <riroan@naver.com>
2022-04-17 05:43:22 +0900

    현재 위치정보 리덕스 적용

--- components/Map.js
+++ components/Map.js
@@ -1,58 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
-import NaverMapView, {
-  Circle,
-  Marker,
-  Path,
-  Polyline,
-  Polygon,
-} from 'react-native-nmap';
+import NaverMapView, {Marker} from 'react-native-nmap';
+import {useAppDispatch} from '../app/hooks';
+import {setLocation} from '../reducers/location.reducer';
 
 export default function Map() {
-  const P0 = {latitude: 37.564362, longitude: 126.977011};
-  const P1 = {latitude: 37.565051, longitude: 126.978567};
-  const P2 = {latitude: 37.565383, longitude: 126.976292};
+  const [data, setData] = useState([
+    {latitude: 37.564362, longitude: 126.977011},
+    {latitude: 37.565051, longitude: 126.978567},
+    {latitude: 37.565383, longitude: 126.976292},
+  ]);
+  const dispatch = useAppDispatch();
 
   return (
     <View>
       <NaverMapView
         style={{width: '100%', height: '100%'}}
         showsMyLocationButton={true}
-        center={{...P0, zoom: 16}}
+        center={{...{latitude: 37.564362, longitude: 126.977011}, zoom: 16}}
         onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-        onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-        onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
-        <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
-        <Marker
-          coordinate={P1}
-          pinColor="blue"
-          onClick={() => console.warn('onClick! p1')}
-        />
-        <Marker
-          coordinate={P2}
-          pinColor="red"
-          onClick={() => console.warn('onClick! p2')}
-        />
-        <Path
-          coordinates={[P0, P1]}
-          onClick={() => console.warn('onClick! path')}
-          width={10}
-        />
-        <Polyline
-          coordinates={[P1, P2]}
-          onClick={() => console.warn('onClick! polyline')}
-        />
-        <Circle
-          coordinate={P0}
-          color={'rgba(255,0,0,0.3)'}
-          radius={200}
-          onClick={() => console.warn('onClick! circle')}
-        />
-        <Polygon
-          coordinates={[P0, P1, P2]}
-          color={`rgba(0, 0, 0, 0.5)`}
-          onClick={() => console.warn('onClick! polygon')}
-        />
+        onCameraChange={e => {
+          dispatch(setLocation({latitude: e.latitude, longitude: e.longitude}));
+        }}
+        onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
+        useTextureView>
+        {data.map((value, ix) => {
+          return (
+            <Marker
+              key={ix}
+              coordinate={value}
+              onClick={() => console.warn(`onClick! p${ix}`)}
+            />
+          );
+        })}
       </NaverMapView>
     </View>
   );
