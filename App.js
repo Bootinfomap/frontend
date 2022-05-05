45f26ebc8c9228b3b030d1f76f0dd906bd05bc0e
riroan <riroan@naver.com>
2022-05-06 08:18:04 +0900

    로그인 화면 추가

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './screens/MainScreen';
+import LoginScreen from './screens/LoginScreen';
 import {store} from './app/store';
 import {Provider} from 'react-redux';
 
@@ -11,7 +12,8 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Main">
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Main" component={MainScreen} />
         </Stack.Navigator>
       </NavigationContainer>
--- app/store.js
+++ app/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import locationReducer from '../reducers/location.reducer';
+import tokenReducer from '../reducers/token.reducer';
 
 export const store = configureStore({
   reducer: {
     location: locationReducer,
+    token: tokenReducer,
   },
 });
